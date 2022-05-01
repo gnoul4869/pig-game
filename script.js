496c80c4eb096df8bd1a0c06080d2dcd030427c6
gnoul_ <ttluong4869@gmail.com>
2022-05-02 03:49:58 +0700

    Update script and rename folder

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 'use strict';
 
-// Selecting elements
-let scoreP0 = document.querySelector('#score-0');
-let scoreP1 = document.querySelector('#score-1');
+// Initial values
+let score1 = 0;
+let score2 = 0;
+let isPlayerOne = true;
 
+// Selecting main elements
+const scoreD1 = document.querySelector('#score-1');
+const scoreD2 = document.querySelector('#score-2');
+const dice = document.querySelector('.dice');
+
+// Selecting buttons
+const btnNew = document.querySelector('.btn-new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn-hold');
+
+// Small functions
+const setScore = (score) => {
+    if (isPlayerOne) {
+        score1 = score;
+        scoreD1.textContent = score;
+    } else {
+        score2 = score;
+        scoreD2.textContent = score;
+    }
+};
+
+// Roll dice
+btnRoll.addEventListener('click', () => {
+    const diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    dice.classList.remove('hidden');
+    dice.src = `assets/dice-${diceNumber}.png`;
+
+    if (diceNumber === 1) {
+        if (isPlayerOne) {
+            setScore(0);
+            isPlayerOne = false;
+        } else {
+            setScore(0);
+            isPlayerOne = true;
+        }
+    }
+});
+
+// Starting game
 const start = () => {
-    scoreP0.textContent = 0;
-    scoreP1.textContent = 0;
+    scoreD1.textContent = 0;
+    scoreD2.textContent = 0;
+    dice.classList.add('hidden');
 };
 
 start();
