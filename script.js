a4aee7e3ba3cf44fde0f875a49533147aab08696
gnoul_ <ttluong4869@gmail.com>
2022-05-02 03:04:27 +0700

    Update name conventions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting elements
+let scoreP0 = document.querySelector('#score-0');
+let scoreP1 = document.querySelector('#score-1');
+
+const start = () => {
+    scoreP0.textContent = 0;
+    scoreP1.textContent = 0;
+};
+
+start();
